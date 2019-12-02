dd1c067d7c7616271e526aabe0bec0d2ee705d7d
luisalourenco <kikentai@gmail.com>
2019-12-02 09:02:59 +0000

    Day 2, part 1

--- 02/part1.py
+++ 02/part1.py
@@ -6,8 +6,32 @@ def add(a, b):
 def mul(a, b):
     return a * b
 
+def IntCode(sequence):
+    # init positions
+    pc = 0
+    a = sequence[pc + 1]
+    b = sequence[pc + 2]
+    res = sequence[pc + 3]
+
+    while sequence[pc] != 99:       
+        if sequence[pc] == 1:
+            sequence[res] = add(sequence[a], sequence[b])
+        elif sequence[pc] == 2:
+            sequence[res] = mul(sequence[a], sequence[b])
+        pc = pc + 4
+        a = sequence[pc + 1]
+        b = sequence[pc + 2]
+        res = sequence[pc + 3]
+    return sequence
 
 with open(filepath) as fp: 	
     input = fp.readline().strip().split(',')
     input = [int(i) for i in input]
-    print input
+    # replace position 1 with value 12, and replace position 2 with value 2.
+    input[1] = 12
+    input[2] = 2
+    
+    input = IntCode(input)
+    print(input[0])
+
+    

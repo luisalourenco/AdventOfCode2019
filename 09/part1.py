c11a857a62480c5bcc4d7c8cf6be17270cf8ad22
luisalourenco <kikentai@gmail.com>
2019-12-10 08:30:28 +0000

    Day 9 complete

--- 09/part1.py
+++ 09/part1.py
@@ -1,4 +1,4 @@
-filepath = 't.txt' 
+filepath = 'input.txt' 
 
 def add(a, b):
     return a + b
@@ -33,79 +33,72 @@ def equals(a, b):
 def nextStep(opCode):
     if opCode == 1 or opCode == 2 or opCode == 7 or opCode == 8:
          return 4
-    else:
+    elif opCode == 3 or opCode == 4 or opCode == 9:
         return 2
+    else:
+        return 0
+
+def getParameterValue(parameterMode, val, sequence, relative, writeMode = False):
+    if parameterMode == 0 and writeMode:
+        return val
+    if parameterMode == 2 and writeMode:
+        return relative + val
+
+    if parameterMode == 0:
+        return sequence[val] 
+    elif parameterMode == 1:
+        return val
+    elif parameterMode == 2:
+        return sequence[relative + val]
+
 
 # (opCode, a, b, res)
 def treatInput(pc, sequence, relative):
     instruction = str(sequence[pc])
-    instruction = instruction.zfill(4)
+    instruction = instruction.zfill(5)
+    #print(instruction)
 
-    opCode = int(instruction[2:4])
-    leftMode = int(instruction[1])
-    rightMode = int(instruction[0])
+    opCode = int(instruction[3:5])
+    resMode = int(instruction[0])
+    leftMode = int(instruction[2])
+    rightMode = int(instruction[1])    
+    
 
-    print(opCode)
-    print(leftMode)
-    print(rightMode)
+    #print(opCode)
+    #print(leftMode)
+    #print(rightMode)
+    #print(resMode)
 
     if opCode == 99:
-        return (99, 0, 0, 0, relative)
-    if opCode == 9:
-        a =  sequence[pc + 1] 
-        if leftMode == 0:
-            a = sequence[a] 
-        elif leftMode == 2:
-            a = sequence[relative + a]
+        return (99, 0, 0, 0)
 
-        return (opCode, a, 0, 0, relative + a)
+    if opCode == 9:
+        a = sequence[pc + 1] 
+        a = getParameterValue(leftMode, a, sequence, relative)
+
+        return (opCode, a, 0, 0)
     if opCode == 5 or opCode == 6:
         a =  sequence[pc + 1]
         b =  sequence[pc + 2]
-        if leftMode == 0:
-            a = sequence[a]
-        elif leftMode == 2:
-            a = sequence[relative + a]  
-        if rightMode == 0:
-            b = sequence[b]
-        elif rightMode == 2:
-            b = sequence[relative + b] 
-
-        return (opCode, a, b, 0, relative)
+        a = getParameterValue(leftMode, a, sequence, relative)
+        b = getParameterValue(rightMode, b, sequence, relative)
+        
+        return (opCode, a, b, 0)
     if opCode == 1 or opCode == 2 or opCode == 7 or opCode == 8:
         a =  sequence[pc + 1]
         b =  sequence[pc + 2]
-
-        if leftMode == 0:
-            #print(a)
-            #print(sequence[a])
-            if a < 0:
-                return (-1,0,0,0,opCode)
-            
-            a = sequence[a]
-
-        elif leftMode == 2:
-            a = sequence[relative + a]  
-
-        if rightMode == 0:
-            b = sequence[b]
-        elif rightMode == 2:
-            b = sequence[relative + b]  
-
         res = sequence[pc + 3]
+        
+        a = getParameterValue(leftMode, a, sequence, relative)
+        b = getParameterValue(rightMode, b, sequence, relative)
+        res = getParameterValue(resMode, res, sequence, relative, True)
 
-        return (opCode, a, b, res, relative)
-    if opCode == 3 or opCode == 4:
-       
+        return (opCode, a, b, res)
+    if opCode == 3 or opCode == 4:       
         a =  sequence[pc + 1]
-        if leftMode == 0:
-            #print(a)
-            #print(sequence[a])
-            a = sequence[a]
-        elif leftMode == 2:
-            a = sequence[relative + a]  
+        a = getParameterValue(leftMode, a, sequence, relative, opCode == 3)
 
-        return (opCode, a, 0, 0, relative)
+        return (opCode, a, 0, 0)
 
 
 def IntCode(sequence, relative):
@@ -114,46 +107,43 @@ def IntCode(sequence, relative):
     opCode = sequence[pc]
 
     while opCode != 99:  
+        (opCode, a, b, res) = treatInput(pc, sequence, relative)
 
-        (opCode, a, b, res, rel) = treatInput(pc, sequence, relative)
-
-        if opCode != -1:
             
-            if opCode == 1:
-                sequence[res] = add(a, b)
+        if opCode == 1:
+            #print("res pos: "+str(res))
+            sequence[res] = add(a, b)
 
-            elif opCode == 2:
-                sequence[res] = mul(a, b)
+        elif opCode == 2:
+            sequence[res] = mul(a, b)
             
-            elif opCode == 7:
-                sequence[res] = lessThan(a, b)
+        #input
+        elif opCode == 3:
+            sequence[a] = 2
 
-            elif opCode == 8:
-                sequence[res] = equals(a, b)
-
-            elif opCode == 5:
-                pc = jumpIfTrue(a, b, pc)
+        elif opCode == 4:
+            print(a)
             
-            elif opCode == 6:
-                pc = jumpIfFalse(a, b, pc)
+        elif opCode == 5:
+            pc = jumpIfTrue(a, b, pc)
+            
+        elif opCode == 6:
+            pc = jumpIfFalse(a, b, pc)
 
-            elif opCode == 9:
-                relative = a
-                
-            #input
-            elif opCode == 3:
-                a = sequence[pc + 1]
-                sequence[a] = 5
+        elif opCode == 7:
+            sequence[res] = lessThan(a, b)
 
-            elif opCode == 4:
-                a = sequence[pc + 1]
-                print(a)
+        elif opCode == 8:
+            sequence[res] = equals(a, b)            
+
+        elif opCode == 9:
+            relative += a                
+           
         
-        if opCode == -1:
-            pc += nextStep(rel)
-        elif  opCode != 6 and opCode != 5:
+        if  opCode != 6 and opCode != 5:
             pc += nextStep(opCode)
-        
+        #print("pc: "+str(pc))
+        #print(sequence)
 
     return sequence
 
@@ -161,6 +151,8 @@ with open(filepath) as fp:
     relative = 0	
     input = fp.readline().strip().split(',')    
     input = [int(i) for i in input]
+    input += [0]*120
+
 
     input = IntCode(input, relative)
 

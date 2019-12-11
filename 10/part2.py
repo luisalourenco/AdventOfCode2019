f3906e2f871451ddeb4c3e261d5124d1ad4e88e4
luisalourenco <kikentai@gmail.com>
2019-12-11 09:44:44 +0000

    Day 10, part 2

--- 10/part2.py
+++ 10/part2.py
@@ -1,28 +1,39 @@
 import math
 
 def countAsteroidsInLoS(xx, yy, map, angles):
-    angleList = []
+    #angleList = []
     list = []
     for y in range(len(map)):
         for x in range(len(map[y])):
             if map[y][x] == '#':
                 vx = x - xx
                 vy = y - yy 
-                angle = math.atan2(vx, vy)
-       
-                elem = ((x,y), angle)    
-                angleList.append(elem)                             
+                angle = math.atan2(vx, vy)       
                 
-                list.append(angle)
+                elem = (x, y, angle)    
+                #angleList.append( elem )                             
+                
+                list.append(elem)
 
     angles[(xx,yy)] = list
     
     return len(set(list))
 
+def getVisibleAsteroidsFromStation(map, station):
+    
+    angles = {} 
+    countAsteroidsInLoS(station[0], station[1], map, angles)
+    #list(set(angles.get(station)))
 
+    visibleAsteroids = list(set(angles.get(station)))
+    visibleAsteroids.sort(key=lambda tup: tup[2]) 
+    #visibleAsteroids.sort()
+    #print(visibleAsteroids)
+    #visibleAsteroids.reverse()
+    return visibleAsteroids
 
 station = (30, 34)
-filepath = 't.txt' 
+filepath = 'input.txt' 
 with open(filepath) as fp: 	
     line = fp.readline().strip()
     map = []
@@ -44,8 +55,36 @@ with open(filepath) as fp:
     n = max(dict.values())
     print(n)
 
-    #print(dict)
-    #print(angles)
-    #visibleAsteroids = dict.get(station)
+
+
+    vaporisedAsteroids = []
+    startingAngle = math.atan2(30-30, 25-34)
+
+    
+    currentAngle = startingAngle
+    fullRotation = True
+    while True:
+        # full rotation, get new visible asteroids
+        if currentAngle == startingAngle and fullRotation:
+            visibleAsteroids = getVisibleAsteroidsFromStation(map, station)
+            fullRotation = False
+        # list is ordered in reverse so we can pop
+        currentAngle = visibleAsteroids.pop()
+        #zap asteroid!
+        map[currentAngle[1]][currentAngle[0]] = '.'
+        
+        while True:
+            aux = visibleAsteroids.pop()
+            if aux[2] != currentAngle[2]:
+                visibleAsteroids.append(aux)
+                break
+               
+        vaporisedAsteroids.append(currentAngle)        
+        if len(vaporisedAsteroids) == 200:
+            break
+
+    #print(vaporisedAsteroids)
+    print(vaporisedAsteroids[199])
+
     
             

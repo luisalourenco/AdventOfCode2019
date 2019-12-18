00a4c87fb8d91e68a9b46674089623324e9d6fb4
luisalourenco <kikentai@gmail.com>
2019-12-18 21:06:17 +0000

    WIP

--- 18/part1.py
+++ 18/part1.py
@@ -2,6 +2,7 @@ import time
 from collections import deque
 from itertools import permutations 
 from itertools import combinations
+import sys
 
 def timer(func):
   def wrapper(*args, **kwargs):
@@ -85,19 +86,33 @@ def bfs(graph, start):
     explored = []
     queue = [start]
     steps = 0
+    keysFound = []
     
+
     while queue:
         node = queue.pop(0)
+        steps+=1
 
-        if node not in explored:
+        if node not in explored:            
+
+            x,y = node
+            nodeElem = map[y][x]
+            if str.islower(nodeElem):
+                keysFound.append(nodeElem)
+            
+            elif str.isupper(nodeElem) and str.lower(nodeElem) not in keysFound:
+                    queue.append(node)
+                    continue
+
             explored.append(node)
             neighbours = graph.get(node)
 
             # add neighbours of node to queue
             for neighbour in neighbours:
                 queue.append(neighbour)
-
-    return steps
+    
+    printElementPath(explored)
+    return steps, explored
 
 def printMap(map, fileMode = True):
     if fileMode:
@@ -148,6 +163,29 @@ def printGraph(graph):
         if nodes != None:
             print(str(k) + " => " + str(nodes))
 
+def printDbg(msg, debug=False):
+    if debug:
+        print(msg)
+
+
+@timer
+def part1_v2(map, debug=False):
+    keys, doors, entrance = gatherLocations(map)
+    graph = buildGraph(map)
+
+    #candidates = permutations(keys)
+
+
+    steps, path = bfs(graph, entrance)
+    print(steps)
+    print(path)
+
+def printElementPath(path):
+    str = ""
+    for x,y in path:
+        str += ", ".join(map[y][x])
+    print(str)
+
 @timer
 def part1(map, debug=False):
     keys, doors, entrance = gatherLocations(map)
@@ -158,24 +196,28 @@ def part1(map, debug=False):
     #print(all)
 
     l = permutations(all)
-    
-    if not debug:
-        start = entrance
-        result = []
-        for p in l:
-            sum = 0
-            start = entrance
-            #print("permutation: "+ str(p))
-            for end in p:
-                path = find_shortest_path(graph, start, end)    
-                #print("distance between "+ str(start) + " and " + str(end) + ": " + str(len(path))  )
 
-                sum += len(path) - 1
-                start = end
-            #print("permutation had "+ str(sum) + " steps")
-            #print("=====================")
-            result.append(sum)
-        return result
+    start = entrance
+
+    result = []
+    for p in l:
+        sum = 0
+        start = entrance
+
+        printDbg("permutation: "+ str(p), debug)
+        for end in p:
+            path = find_shortest_path(graph, start, end)  
+            
+
+            printDbg("distance between "+ str(start) + " and " + str(end) + ": " + str(len(path)), debug  )
+                
+            sum += len(path) - 1
+            start = end
+        printElementPath(p)
+        printDbg("permutation had "+ str(sum) + " steps", debug)
+        printDbg("=====================", debug)
+        result.append(sum)
+    return result
 
 
 
@@ -201,5 +243,9 @@ with open(filepath) as fp:
 
     printMap(map)
 
-    result = part1(map)
-    print(min(result))
+    debug = False
+    if len(sys.argv) == 2:
+        debug = sys.argv[1]
+    
+    result = part1_v2(map, debug)
+    #print(min(result))

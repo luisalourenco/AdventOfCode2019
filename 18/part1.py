d327dd10dcf849bdb9ce8583adbdd4d308391f76
luisalourenco <kikentai@gmail.com>
2019-12-18 17:36:16 +0000

    WIP

--- 18/part1.py
+++ 18/part1.py
@@ -13,9 +13,12 @@ def timer(func):
 
 def buildGraph(map):
     graph = {}
+    sizeX = len(map[0])
+    sizeY = len(map)
 
-    for y in range(1,99):
-        for x in range(1,99):
+
+    for y in range(1,sizeY-1):
+        for x in range(1,sizeX-1):
 
             east = (x+1, y)
             west = (x-1, y)
@@ -23,14 +26,15 @@ def buildGraph(map):
             south = (x, y+1)
             
             neighbours = []
-            if map[east[1]][east[0]] != ' ' and map[east[1]][east[0]] != '#':
-                neighbours.append(east)
-            if map[west[1]][west[0]] != ' ' and map[west[1]][west[0]] != '#':
-                neighbours.append(west)
-            if map[north[1]][north[0]] != ' ' and map[north[1]][north[0]] != '#':
-                neighbours.append(north)
-            if map[south[1]][south[0]] != ' ' and map[south[1]][south[0]] != '#':
-                neighbours.append(south)
+            if map[y][x] != ' ' and map[y][x] != '#':
+                if map[east[1]][east[0]] != ' ' and map[east[1]][east[0]] != '#':
+                    neighbours.append(east)
+                if map[west[1]][west[0]] != ' ' and map[west[1]][west[0]] != '#':
+                    neighbours.append(west)
+                if map[north[1]][north[0]] != ' ' and map[north[1]][north[0]] != '#':
+                    neighbours.append(north)
+                if map[south[1]][south[0]] != ' ' and map[south[1]][south[0]] != '#':
+                    neighbours.append(south)
             
             graph[(x,y)] = neighbours
     return graph
@@ -107,50 +111,95 @@ def printMap(map, fileMode = True):
 
 def gatherLocations(map):
     keys = []
-    doors = {}
+    doors = []
     entrance = None
-    for j in range(100):
-        for i in range(100):
+    sizeX = len(map[0])
+    sizeY = len(map)
+
+    for j in range(sizeY):
+        for i in range(sizeX):
             obj = map[j][i]
             if obj != '@':
                 if str.islower(obj):
                     #keys[obj] = (i,j)                    
                     keys.append((i,j))
                 elif str.isupper(obj):
-                    doors[obj] = (i,j)
+                    #doors[obj] = (i,j)
+                    doors.append((i,j))
             else:
                 entrance = (i,j)
     return keys, doors, entrance
 
+def find_shortest_path2(graph, start, end):
+        dist = {start: [start]}
+        q = [start]
+        while len(q):
 
-map = [ [ ('') for i in range(100) ] for j in range(100) ] 
-filepath = 't.txt' 
-with open(filepath) as fp: 	
-    line = fp.readline().strip()
-    
-    j = 0
-    while line:
-        for i in range(len(line)):
-            map[j][i] = line[i]
-        j += 1
-        line = fp.readline().strip()
-    #end while
+            at = q.pop(0)            
+            for next in graph.get(at):
+                if next not in dist:
+                    dist[next] = [dist[at], next]
+                    q.append(next)
+        return dist.get(end)
 
-    printMap(map)
+def printGraph(graph):
+    for k in graph:
+        nodes = graph.get(k)
+        if nodes != None:
+            print(str(k) + " => " + str(nodes))
+
+@timer
+def part1(map, debug=False):
     keys, doors, entrance = gatherLocations(map)
     graph = buildGraph(map)
 
-    print(len(keys))
-    l = permutations(keys)
-    start = entrance
-    result = []
-    for p in l:
-        sum = 0
+    all = keys
+    all.sort()
+    #print(all)
+
+    l = permutations(all)
+    
+    if not debug:
         start = entrance
-        for end in p:
-            path = find_shortest_path(graph, start, end)
-            if path != None:
-                sum += len(path)
-            start = end
-        result.append(sum)
+        result = []
+        for p in l:
+            sum = 0
+            start = entrance
+            #print("permutation: "+ str(p))
+            for end in p:
+                path = find_shortest_path(graph, start, end)    
+                #print("distance between "+ str(start) + " and " + str(end) + ": " + str(len(path))  )
+
+                sum += len(path) - 1
+                start = end
+            #print("permutation had "+ str(sum) + " steps")
+            #print("=====================")
+            result.append(sum)
+        return result
+
+
+
+
+filepath = 't.txt' 
+with open(filepath) as fp: 	
+    line = fp.readline().strip()
+    size = len(line)
+    aux = [ [ ('') for i in range(size) ] for j in range(size) ]     
+    j = 0
+    while line:
+        for i in range(size):
+            aux[j][i] = line[i]
+        j += 1
+        line = fp.readline().strip()
+    #end while
+    map = [ [ ('') for i in range(size) ] for j in range(j) ]     
+    
+    #copy to correct size structure
+    for y in range(j):
+        for x in range(size):
+            map[y][x] = aux[y][x]
+
+    printMap(map)
+
+    result = part1(map)
     print(min(result))

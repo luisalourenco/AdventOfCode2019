87fa0dc94698742955686806bbb332b7184a4dff
luisalourenco <kikentai@gmail.com>
2019-12-23 19:33:52 +0000

    Day 23, part 2

--- 23/part2.py
+++ 23/part2.py
@@ -111,40 +111,6 @@ def treatInput(pc, sequence, relative):
 
         return (opCode, a, 0, 0)
 
-def moveForward(robot, direction):
-    x = robot[0]
-    y = robot[1]
-    # north
-    if direction == '1':
-        return (x, y - 1)
-    # east        
-    elif direction == '4':
-        return (x + 1, y)  
-    # south  
-    elif direction == '2':
-        return (x, y + 1)
-    # west     
-    elif direction == '3':
-        return (x - 1, y)    
-    
-def pickDirection(map, x, y):
-    # if unexplored, go!
-    
-    if map[y - 1][x] == ' ':
-        return 1
-    if map[y + 1][x] == ' ':
-        return 2
-    if map[y][x - 1] == ' ':
-        return 3
-    if map[y][x + 1] == ' ':
-        return 4
-
-    return random.randint(1,4)
-
-def wait(sequence, a, inParam, pc, comp):
-    sequence[a] = inParam 
-    print("switching computer for "+ str(comp))
-    return (pc+2, sequence)
 
 def IntCode(sequence, relative, inParam, queues, states, nat, idleVal, idle, init = False):
     # init positions
@@ -153,7 +119,7 @@ def IntCode(sequence, relative, inParam, queues, states, nat, idleVal, idle, ini
     #obtain state
     pc = states.get(comp)[0]
     sequence = states.get(comp)[1]
-    print("[" + str(comp)+"] PC: " +str(pc))
+    #print("[" + str(comp)+"] PC: " +str(pc))
 
     opCode = sequence[pc]
     
@@ -181,17 +147,14 @@ def IntCode(sequence, relative, inParam, queues, states, nat, idleVal, idle, ini
             if not init:
                 # read queue for this computer
                 queue = queues.get(comp)
-                if idle and comp == 0:
-                    print("setting idle value for " + str(comp)+ ": "+str(idleVal))
-                    queue = [idleVal]
-                    idle = False
 
                 if len(queue) == 0:
-                    if sent >= 30:
+                    # wait 100 iterations before pausing
+                    if sent >= 100:
                         sequence[a] = inParam 
-                        print("[" + str(comp)+"] Switching computer")
+                        print("Pausing computer [" + str(comp)+"] ")
                         print("================")
-                        return (pc+2, sequence)
+                        return (pc+2, sequence, queues)
                     else:
                         inParam = -1
                     sent +=1
@@ -199,31 +162,32 @@ def IntCode(sequence, relative, inParam, queues, states, nat, idleVal, idle, ini
 
                     if readX == None: # read X
                         packet = queue[0]
+                        print(packet)
                         inParam = packet[0]
                         readY = packet[1]
                         readX = packet[0]                        
                     else: # read Y, remove packet from queue
-                        print("PACKET RECEIVED! ")
-                        print("Queue: "+str(comp))
-                        print("X: "+str(readX))
-                        print("Y: "+str(readY))
-                        print("=====")
+                        print("<<< ========")
+                        print("PACKET RECEIVED at computer [" + str(comp)+"]! ")
+                        print("X: "+str(readX) +", Y: "+str(readY))
+                        print("<<< ========")
+                        print()
                         queue.pop(0)
                         inParam = readY
                         readX = None
             
             else:
-                print("booting: "+str(inParam))
+                print("Booting computer [" + str(inParam)+"]")
                 # init computer with its address
                 comp = inParam
                 # bootstrap
                 sequence[a] = inParam 
                 # sinalise the bootstrap is completed
                 init = False                
-                print("bootstrap ended for "+ str(comp))
+                print("Bootstrap ended for computer "+ str(comp)+ ".")
 
                 # return current state
-                return (pc+2, sequence)
+                return (pc+2, sequence, queues)
 
             sequence[a] = inParam 
 
@@ -235,34 +199,30 @@ def IntCode(sequence, relative, inParam, queues, states, nat, idleVal, idle, ini
                 x = a
             elif y == None: 
                 y = a
-                
-                if dst == 255:
-                    print("YYYY: "+ str(y))
-
-                    #if (x,y) in nat and idle: #repeating 255
-                    #    return (pc+2, sequence)
-                    nat.add((x,y))
-
+                # get destinatary queue
                 q = queues.get(dst) 
 
-                if dst == 255: # only save last one
-                    q = [(x,y)]          
+                # if destinatary is NAT then replace value in queue
+                if dst == 255: 
+                    print("NAT PACKET: "+ str(y))
+                    q = [(x,y)]   # only save last one  
                 else:
                     q.append((x,y))
                 queues[dst] = q
-                
+
+                print("======== >>>")
                 print("PACKET SENT! ")
                 print("Queue: "+str(dst))
                 print("X: "+str(x))
                 print("Y: "+str(y))
-                print("=====")
+                print("======== >>>")
+                print()
                 sent += 1
                 dst = None
                 x = None
                 y = None
                 
-
-            #print("COMP: " + str(comp) + ", out: "+ str(a))
+                return (pc+2, sequence, queues)
 
         elif opCode == 5:
             pc = jumpIfTrue(a, b, pc)
@@ -279,37 +239,9 @@ def IntCode(sequence, relative, inParam, queues, states, nat, idleVal, idle, ini
         elif opCode == 9:
             relative += a                
            
-        
         if  opCode != 6 and opCode != 5:
             pc += nextStep(opCode)
-        #print("pc: "+str(pc))
-        #print(sequence)
-
     
-    return a
-
-
-def printMap(map, fileMode = True):
-    if fileMode:
-        file1 = open("MyFileAll.txt","w") 
-    
-        for l in map:
-            for j in range(len(l)):
-                file1.write(l[j])
-            file1.write("\n")
-        file1.close() 
-
-
-def printMap2(map, fileMode = True):
-    if fileMode:
-        file1 = open("MyFileAll.txt","w") 
-    
-        for l in map:
-            for j in range(len(l)):
-                file1.write(str(l[j]))
-            file1.write("\n")
-        file1.close() 
-
 
 
 filepath = 'input.txt' 
@@ -326,6 +258,7 @@ with open(filepath) as fp:
     myInput = [int(i) for i in myInput]
     myInput += [0]*10000
 
+    # init vars
     for i in range(50):
         states[i] = (0, myInput.copy())
         queues[i] =  []   
@@ -335,50 +268,57 @@ with open(filepath) as fp:
     idle = False
     # bootstrap
     for addr in range(50):
-        res, seq = IntCode(myInput.copy(), relative, addr, queues, states, nat, None, idle, True)
+        res, seq, q = IntCode(myInput.copy(), relative, addr, queues, states, nat, None, idle, True)
         states[addr] = (res, seq)
+    print("===== END OF BOOTSTRAP =====")
+    print()
 
-    exit = False    
-    # run network
+
+
     inParam = -1
-    idlesTimes = 0
-    for i in range(500):
-        if exit:
-            break
+    idleTimes = 0
+    # iterations of machines executions
+    for i in range(5000):
 
+        # check if network is idle
         if sum(idleMachines.values()) == 50:
-                inParam = queues.get(255).pop(0)
-                print("all machines idle!")               
-                print(inParam)
-                idle = True
-
+                natQueue = queues.get(255)
+                if len(natQueue) != 0:
+                    idleTimes += 1
+                    if idleTimes == 100:
+                        inParam = natQueue.pop(0)
+                        print("Network is idle!")               
+                        print(inParam)
+                        idleTimes = 0
+                        idle = True
+                        print("Sending packet to 0 from NAT. Value " + str(inParam)+ "")
+                        # send NAT packet to computer 0 queue
+                        queues.get(0).append(inParam)
+                        
+        else:
+            idleTimes = 0
+        
+        # run network
         for addr in range(50):
+            print("Resuming computer "+ str(addr)+ ".")
+           
+            # save state
             oldStates[addr] = states.get(addr)
-            res, seq = IntCode(myInput.copy(), relative, addr, queues, states, nat, inParam, idle)  
-            idle = False
             oldState = oldStates.get(addr)
-            
-            
-            if oldState == (res, seq):
-                print("idle: " +str(addr))
-                idleMachines[addr] = 1
-            else:
-                idleMachines[addr] = 0
-
+            # run program until stuck in reading 
+            res, seq, q = IntCode(myInput.copy(), relative, addr, queues, states, nat, inParam, idle)  
             states[addr] = (res, seq)
 
-            queue = queues.get(255)
-            # solution is the first Y value that the NAT *sends* twice.
-            #if len(queue) != 0:
-            #    if queue[0] in nat:
-            #        exit = True
-            #        break
-            #    else:
-            #        nat.add(queue[0])
-                
-                
-    #print(queues)
-    
+            for i in range(50):
+                if len(q.get(i)) != 0:
+                    idleMachines[addr] = 0
+                else:
+                    idleMachines[addr] = 1
+
+            
+            print()
+        #end for
+
     
       
     

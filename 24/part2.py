828b5491d1d5fa415229e19a74b47f251d6337ac
luisalourenco <kikentai@gmail.com>
2019-12-24 18:21:06 +0000

    Day 24, part 2 complete

--- 24/part2.py
+++ 24/part2.py
@@ -1,8 +1,4 @@
 import time
-from collections import deque
-from itertools import permutations 
-from itertools import combinations
-import sys
 
 def timer(func):
   def wrapper(*args, **kwargs):
@@ -34,29 +30,53 @@ def printMap(map, level = None, iteration = None, fileMode = True):
         file1.close() 
 
 
-def bugsInAdjacentTilesLowerLevel(map, x, y):
+def bugsInAdjacentTilesLowerLevel(map, border):
     # -1, 0 - lower border
     # 0, -1 - upper border
     # -1, -1 - right border
     # -2, -2 - left border
 
     bugs = 0
-    # implement borders logic above
-    if x == -1 and y == 0:
+    if border == 'DOWN':
         for i in range(5):
             if map[4][i] == '#':
                 bugs += 1
-    if x == 0 and y == -1:
+    if border == 'UP':
         for i in range(5):
             if map[0][i] == '#':
-                    bugs += 1
-    if x == -1 and y == -1:
+                bugs += 1
+    if border == 'RIGHT':
         for i in range(5):
             if map[i][4] == '#':
                 bugs += 1
-    if x == -2 and y == -2:
+    if border == 'LEFT':
         for i in range(5):
             if map[i][0] == '#':
+                bugs += 1
+    return bugs
+
+def bugsInAdjacentTilesUpperLevel(map, x, y):
+    bugs = 0
+    if map != None:
+        left = map[2][1]
+        right = map[2][3]
+        up = map[1][2]
+        down = map[3][2] 
+
+        if x == 0:
+            if left == '#':
+                bugs += 1
+
+        if y == 0:
+            if up == '#':
+                bugs += 1
+
+        if x == 4:
+            if right == '#':
+                bugs += 1
+
+        if y == 4:
+            if down == '#':
                 bugs += 1
     return bugs
 
@@ -64,119 +84,96 @@ def bugsInAdjacentTiles(map, x, y, levels, level):
     bugs = 0
     size = 5
 
-    topMap = levels.get(level + 1)
-    if topMap != None:
-        lefTop = topMap[2][1]
-        rightTop = topMap[2][3]
-        top = topMap[1][2]
-        bot = topMap[3][2]
-        # implement upper level logic
-        # if x = 0, compare with x,y? LeftTop
-        # or x = 5,  ?x,y rightTop
-        # if y = 0, ? top x,y Top
-        # or y = 5 x,y ? bot Bot
-        if x == 0:
-            if lefTop == '#':
-                bugs += 1
-        if y == 0:
-            if top == '#':
-                bugs += 1
-        if x == 4:
-            if rightTop == '#':
-                bugs += 1
-        if y == 4:
-            if bot == '#':
-                bugs += 1
+    upperMap = levels.get(level + 1)   
+    bugs += bugsInAdjacentTilesUpperLevel(upperMap, x, y)
+
+    lowerLevel = levels.get(level-1)
+    if lowerLevel != None:
+        if (x,y) == (2,1): # upper
+            bugs += bugsInAdjacentTilesLowerLevel(lowerLevel, 'UP')
+        if (x,y) == (1,2): # left
+            bugs += bugsInAdjacentTilesLowerLevel(lowerLevel, 'LEFT')
+        if (x,y) == (3,2): # right
+            bugs += bugsInAdjacentTilesLowerLevel(lowerLevel, 'RIGHT')
+        if (x,y) == (2,3): # down
+            bugs += bugsInAdjacentTilesLowerLevel(lowerLevel, 'DOWN')
 
     if y != 0:
         if map[y-1][x] == '#': 
             bugs += 1
-        if map[y-1][x] == '?':
-            lowerLevel = levels.get(level-1)
-            if lowerLevel != None:
-                # search lower level in lower border
-                bugs += bugsInAdjacentTilesLowerLevel(lowerLevel, -1, 0)
     
     if y != size -1:
         if map[y + 1][x] == '#':
             bugs += 1
-        if map[y + 1][x] == '?':
-            lowerLevel = levels.get(level-1)
-            if lowerLevel != None:
-                # search lower level in upper border
-                bugs += bugsInAdjacentTilesLowerLevel(lowerLevel, 0, -1)
     
     if x != 0:
         if map[y][x - 1] == '#':
             bugs += 1
-        if map[y][x - 1] == '?':
-            lowerLevel = levels.get(level-1)
-            if lowerLevel != None:
-                # search lower level in right border
-                bugs += bugsInAdjacentTilesLowerLevel(lowerLevel, -1, -1)
     
     if x != size -1:
         if map[y][x + 1] == '#':
             bugs += 1
-        if map[y][x + 1] == '?':
-            lowerLevel = levels.get(level-1)
-            if lowerLevel != None:
-                # search lower level in left border
-                bugs += bugsInAdjacentTilesLowerLevel(lowerLevel, -2, -2)
+        
     return bugs
 
 
 def mutation(levels, levelSize, iteration):
     size = 5
-    #level = 0
     newLevels = {}
 
-    for level in range(-levelSize,levelSize):
+    for level in range(-levelSize, levelSize):
+        # current map
         map = levels.get(level)
-
+        # map after mutation
         newMap = [ [ '.' for i in range(5) ] for j in range(5) ]
         
         for y in range(size):
             for x in range(size):
                 unchanged = True
-                numBugs = bugsInAdjacentTiles(map, x, y, levels, level)
+                if (x,y) != (2,2):
+                    # count bugs in adjacent tiles, including edge cases with lower and upper level
+                    numBugs = bugsInAdjacentTiles(map, x, y, levels, level)
 
-                if map[y][x] == '#' and numBugs != 1:
-                    newMap[y][x] = '.'
-                    unchanged = False      
-                    
-                if map[y][x] == '.' and (numBugs == 1 or numBugs == 2):
-                    newMap[y][x] = '#'
-                    unchanged = False
+                    # bug dies unless there is exactly one bug adjacent to it
+                    if map[y][x] == '#' and numBugs != 1:
+                        newMap[y][x] = '.'
+                        unchanged = False      
+                    # empty space becomes infested with a bug if exactly one or two bugs are adjacent to it
+                    if map[y][x] == '.' and (numBugs == 1 or numBugs == 2):
+                        newMap[y][x] = '#'
+                        unchanged = False
 
+                    # otherwise, nothing changes
                 if unchanged:
                     newMap[y][x] = map[y][x]
             #end for
         #end for
+        # update current level's map
         newLevels[level] = newMap.copy()
+
+        # printing map for debugging
         if level == -levelSize:
             printMap(newMap, level, iteration+1)
         else:
             printMap(newMap, level)
-        #level += 1
 
-    return (newMap, newLevels)
+    return newLevels
 
-def biodiversityRating(map):
-    num = 0
-    rating = 0
-    for y in range(5):
-        for x in range(5):
-            if map[y][x] == '#':
-                rating += pow(2, num)
-            num +=1
-    return rating
 
+def countBugsAllLevels(levels):
+    bugs = 0
+    for map in levels.values():
+        for y in range(5):
+            for x in range(5):
+                if map[y][x] == '#':
+                    bugs += 1
+    return bugs
+
+@timer
 def part2(map, iterations, size):
     levels = {}
-    resultMap = map    
 
-    # init levels maps for 1..100
+    # init levels maps for -size..size
     for i in range(-size, size):
         level = [ [ '.' for i in range(5) ] for j in range(5) ] 
         level[2][2] = '?'        
@@ -184,18 +181,16 @@ def part2(map, iterations, size):
 
     levels[0] = map
     newLevels = levels.copy()
+
     for i in range(iterations):
-        #printMap(levels.get(0))
-        (resultMap, newLevels) = mutation(newLevels, size, i)
+        # feed next iteration with resulting levels maps
+        newLevels = mutation(newLevels, size, i)
     
-    #for 
+    # count bugs for all levels
+    print(countBugsAllLevels(newLevels)) 
     
-    #printMap(newLevels.get(0))
 
-    return resultMap
-
-
-filepath = 't.txt' 
+filepath = 'input.txt' 
 with open(filepath) as fp: 	
     line = fp.readline().strip()
     map = [ [ '.' for i in range(5) ] for j in range(5) ] 
@@ -207,8 +202,8 @@ with open(filepath) as fp:
         line = fp.readline().strip()
     #end while
 
-    
-    m = part2(map, 10, 5)
+    # part2(map, iterations, size)
+    part2(map, 200, 110)
 
     
    
